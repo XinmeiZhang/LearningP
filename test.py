353506a3d82214cd5b6a3619006a6e5418c37604
Teilnehmer X <Teilnehmer@PC10.VHS.local>
2020-03-05 18:59:15 +0100

    erste Schritte im Python Kurs

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+a= input()
+b= input()
+c= int(a)+int(b)
+print(c)
+
+
+for i in range(5):
+    print(c)
+    
+if a == b:
+    print("nice")
+else:
+        print("awww")
+
+list(ran) 
