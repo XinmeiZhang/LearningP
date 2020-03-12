36f110aaaa5d1c463714f73a55dc143ba5a2555d
Teilnehmer X <Teilnehmer@PC10.VHS.local>
2020-03-12 20:04:08 +0100

    ssss

---
+++ Unterricht2e.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 19:46:19 2020
+
+@author: Teilnehmer
+"""
+
+# here we call only the hello function
+#from Unterricht2e_hello import hello
+#hello()
+
+# here we call the whole hello 
+import Unterricht2e_hello
+Unterricht2e_hello.hello
+
+from math import sqrt
+cc = sqrt(25)
+
+import math
+dd = math.sqrt(35)
+
+import numpy
+ee = numpy.sqrt(35)
---
+++ Unterricht2e_hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 19:45:47 2020
+
+@author: Teilnehmer
+"""
+
+def hello():
+    print("hello")
+    
+hello()
